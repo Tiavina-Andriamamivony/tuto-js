32bd754684e729e1ee9f55719cb62d6d4a58c398
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-06-18 19:56:31 +0300

    chore: initial commit.

---
+++ variable.js
@@ -0,0 +1,19 @@
+console.log("Hello, World!");
+
+
+// Variable declaration
+
+let myVariable = 42; // This is a number
+const myConstant = "This is a constant"; // This is a string
+var myOldVariable = true;
+
+console.log(myVariable); // Output: 42
+
+myVariable= 44;
+
+console.log(myVariable); // Output: 44
+
+
+
+
+//Manambotra système d'authentification simple 
